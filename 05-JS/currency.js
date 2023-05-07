546d4d2fa544568e36adbe29079330c9e4f33d12
torbjorns <torbjorn.sitre@gmail.com>
2023-05-07 07:56:18 +0200

    Done with currency

--- 05-JS/currency.js
+++ 05-JS/currency.js
@@ -1,14 +1,50 @@
+var myHeaders = new Headers();
+myHeaders.append("apikey", "s6xpxJPpHpWJHatIyad9LNJEoYkU3DOZ");
 
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Send a GET request to the URL
-    fetch('https://api.exchangeratesapi.io/latest?base=USD')
-    // Put response into json form
-    .then(response => response.json())
-    .then(data => {
-        // Log data to the console
-        console.log(data);
-    });
+    document.querySelector('form').onsubmit = function() {
 
+        var myHeaders = new Headers();
+        myHeaders.append("apikey", "s6xpxJPpHpWJHatIyad9LNJEoYkU3DOZ");
 
+        var requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+        };
+
+        // Send a GET request to the URL
+        fetch("https://api.apilayer.com/exchangerates_data/latest?base=USD", requestOptions)
+        // Put response into json form
+        .then(response => response.json())
+        .then(data => {
+            // Get currency from user input and convert to upper case
+            const currency = document.querySelector('#currency').value.toUpperCase();
+
+            // Get rate from data
+            const rate = data.rates[currency];
+
+            // Check if currency is valid:
+            if (rate !== undefined) {
+                // Display exchange on the screen
+                document.querySelector('#result').innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}.`;
+            }
+            else {
+                // Display error on the screen
+                document.querySelector('#result').innerHTML = 'Invalid Currency.';
+            }
+        })
+        // Catch any errors and log them to the console
+        .catch(error => {
+            console.log('Error:', error);
+        });
+        // Prevent default submission
+        return false;
+    }
 });
