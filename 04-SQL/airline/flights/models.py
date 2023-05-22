15fcde5ec3424f0381555cd03601e1bf6e24e2a7
Torbjørn Sitre <torbjorn.sitre@gmail.com>
2023-05-22 14:44:51 +0200

    "Use wrong valid flight function"

--- 04-SQL/airline/flights/models.py
+++ 04-SQL/airline/flights/models.py
@@ -21,7 +21,7 @@ class Flight(models.Model):
     
     # Check if it is a valid flight
     def is_valid_flight(self):
-        return self.origin != self.destination and self.duration > 0
+        return self.origin != self.destination or self.duration > 0
     
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
